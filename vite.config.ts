6cd654ff8ff208f4c1b5175b9285ca4e54060d97
Mani Selvam <144051889+Mani-Selvam@users.noreply.github.com>
2025-12-11 13:04:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig(({ mode }) => {
         },
         root: path.resolve(import.meta.dirname, "client"),
         build: {
-            outDir: path.resolve(import.meta.dirname, "dist"),
+            outDir: path.resolve(import.meta.dirname, "dist","public"),
             emptyOutDir: true,
         },
         server: {
